7faba39f3d9aaf34a4972a7abfb10f0698c24664
IoT4Env <ioan.matteo253@gmail.com>
2024-07-21 15:21:01 +0200

    Organized opcua server logic inside dedicated class

---
+++ Server/opcuaServerLib.js
@@ -0,0 +1,41 @@
+import { OPCUAServer, Variant, DataType, StatusCodes } from "node-opcua"
+
+export class OpcUaServer {
+
+    constructor() {
+        this.server = this.createServer()
+    }
+
+    createServer = _ => new OPCUAServer({
+        port: 4334,
+        resourcePath: "/UA/UrbanMining",
+        buildInfo: {
+            productName: "UrbanMiningServer",
+            buildNumber: "7658",
+            buildDate: new Date()
+        }
+    })
+
+    initServer = async _ => {
+        await this.server.initialize()
+    }
+
+    startServer = async _ => {
+        await this.server.start()
+        const endpointUrl = this.server.getEndpointUrl();
+        console.log("Server is now listening ... ( press CTRL+C to stop)");
+        console.log(" the primary server endpoint url is ", endpointUrl);    
+    }
+
+    stopServerListener = _ =>{
+        process.once('SIGINT', async _=>{
+            console.log('shutting down')
+            this.server.shutdown()
+        })
+    }
+
+    getOwnNamespace =  _ => {
+        const addressSpace = this.server.engine.addressSpace
+        return addressSpace.getOwnNamespace();
+    }
+}
