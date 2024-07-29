192862be40fde21b0eb82d4a3b44590ec63ffcdb
IoT4Env <ioan.matteo253@gmail.com>
2024-07-29 16:34:11 +0200

    Got instances created in the opc ua server

--- Client/opcuaClientLib.py
+++ Client/opcuaClientLib.py
@@ -1,5 +1,6 @@
 from opcua import Client
-
+# from opcua import UaClient
+# from opcua.client.ua_client import UaClient
 
 class opcuaClient:
 
@@ -12,5 +13,11 @@ class opcuaClient:
     def get_root_node(self):
         return self.client.get_root_node()
 
+    #get_browse_name works only on the instance (returns the name) and on properties of instance (returns the name)
+    def get_own_instances(self):
+        objects = self.client.get_objects_node()#get ALL objects inside the object node (it is a folder)
+        children = objects.get_children()#get all istances inside the object node
+        return children[3:len(children)]#ours instances can be found past the third element
+    
     def disconnect(self):
         self.client.disconnect()
--- Server/weeeServer.js
+++ Server/weeeServer.js
@@ -1,3 +1,4 @@
+import { DataType } from "node-opcua";
 import { OpcUaServer } from "./opcuaServerLib.js";
 
 
@@ -10,19 +11,49 @@ import { OpcUaServer } from "./opcuaServerLib.js";
     const addressSpace = server.getAddressSpace()
     const namespace = addressSpace.getOwnNamespace();
 
+
+    //#region Enums definition
+    const weightEnum = ["High", "Medium", "Low"]
+    const weightPlatforms = namespace.addEnumerationType({
+        browseName: "Weights",
+        enumeration: weightEnum
+    })
+
+    //#endregion
+
     //#region  Type definition
     const plcType = namespace.addObjectType({
         browseName: "PlcType"
     })
-    //#endregion
 
-
-    plcType.instantiate({
-        browseName: "plc1",
-        organizedBy: addressSpace.rootFolder.objects
+    namespace.addVariable({
+        browseName: "Id",
+        componentOf: plcType,
+        dataType: DataType.Int16,
+        modellingRule: "Mandatory",
+        value:{dataType: DataType.Int16, value: 24}
     })
 
-    
+    namespace.addVariable({
+        browseName: "PowerConsuption",
+        componentOf: plcType,
+        dataType: DataType.Double,
+        modellingRule: "Mandatory",
+        value:{dataType: DataType.Double, value: 12.4}
+    })
+    //#endregion
+
+    const clensePlc = plcType.instantiate({
+        browseName: "ZClensePlc",
+        organizedBy: addressSpace.rootFolder.objects,
+        })
+
+    const clensePlc2 = plcType.instantiate({
+        browseName: "AClensePlc2",
+        organizedBy: addressSpace.rootFolder.objects,
+        })
+
+
     await server.startServer()
     server.stopServerListener()
 })()
