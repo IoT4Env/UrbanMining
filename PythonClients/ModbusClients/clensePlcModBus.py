821d387a7b923138cf1a8b8c485c1f3552a2a57b
IoT4Env <170166502+IoT4Env@users.noreply.github.com>
2025-08-11 13:24:59 +0200

    Experimented with openplc editor and found a way to expose varibles for modbus

--- PythonClients/ModbusClients/clensePlcModBus.py
+++ PythonClients/ModbusClients/clensePlcModBus.py
@@ -8,8 +8,9 @@ clensePlc_client = ModbusTcpClient(host=slave_address, port=port)
 
 if __name__ == '__main__':
     clensePlc_client.connect()
+    print(f'connected to {clensePlc_client}')
 
-    read2 = clensePlc_client.read_holding_registers(0,2)
+    read2 = clensePlc_client.read_holding_registers(10, 3)
     print(read2.registers)
 
     clensePlc_client.close()
