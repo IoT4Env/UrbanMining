7a1db6fd6b35dfc1cf59162533fcc2ec45a60fc4
IoT4Env <170166502+IoT4Env@users.noreply.github.com>
2025-08-11 13:24:59 +0200

    Configured plc logic for all attached  platforms + better code organization

--- PythonClients/ModbusClients/weightPlcModBus.py
+++ PythonClients/ModbusClients/weightPlcModBus.py
@@ -11,23 +11,29 @@ if __name__ == '__main__':
     weightPlc_client.connect()
     print(f'connected to {weightPlc_client}')
 
-    id = weightPlc_client.read_holding_registers(3, 1)#starting at 0, read x registers
+    id = weightPlc_client.read_holding_registers(1, 1)#starting at 0, read x registers
     print(f'Reading PLC id number {id.registers}')#expect to read id 3
     
-    #Read power consuption when plc is closed
-    pc = weightPlc_client.read_holding_registers(100, 1)
-    print(f'Current power consuption: {pc.registers}')
-
     #Set status to Start
-    weightPlc_client.write_register(110, 0)#Chnage second argument to 0 or 1 to close or start plc
+    weightPlc_client.write_register(110, 1)#Chnage second argument to 0 or 1 to close or start plc
     status = weightPlc_client.read_holding_registers(110, 1)#read status of first plc
     print(f'Status set to: {status.registers}')
     time.sleep(0.1)#wait some time before reading the updated value
     
-
     #Read power consuption and see it changed
     pc = weightPlc_client.read_holding_registers(100, 1)
     print(f'Current power consuption: {pc.registers}')
+
+
+    #command weight h platform
+    weightPlc_client.write_register(113, 0)
+    status_weight_h_platform = weightPlc_client.read_holding_registers(113, 1)#read status of first plc
+    print(f'Status of weight platform set to: {status_weight_h_platform.registers}')
+    time.sleep(0.1)#wait some time before reading the updated value
+
+    #read pc of weight h platform
+    pc_weight_h_platform = weightPlc_client.read_holding_registers(102, 1)
+    print(f'Power consuption of weight platform is: {pc_weight_h_platform.registers}')
 
     weightPlc_client.close()
 
